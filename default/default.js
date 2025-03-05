85a406c3c41f81321cc00d12b7df22c33ef91931
usamaSRT <usamasystemzreitechnolgy@gmail.com>
2025-03-05 14:41:04 +0900

    Add files via upload

---
+++ default/default.js
@@ -0,0 +1,8 @@
+const SRT0 = document.querySelectorAll(".js_button");
+const SRT1 = document.body;
+SRT0.forEach(SRT0Z =>{
+    SRT0Z.addEventListener("click",SRT1Z);
+});
+function SRT1Z(){
+    SRT1.classList.toggle("open");
+}
